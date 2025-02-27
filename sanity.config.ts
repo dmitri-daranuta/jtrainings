b3e2fe11177830e1eea9ba6dd6570b4b1a27d09c
Dmitri Daranuta <dmitri.daranuta@ffw.com>
2025-02-27 11:27:08 +0200

    Implementing Sanity’s Presentation Mode

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,13 +7,13 @@
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
+import {presentationTool} from 'sanity/presentation'
+import {codeInput} from '@sanity/code-input'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './sanity/env'
 import {schema} from './sanity/schemaTypes'
 import {structure} from './sanity/structure'
-
-import {codeInput} from '@sanity/code-input'
 
 export default defineConfig({
   basePath: '/studio',
@@ -24,8 +24,21 @@ export default defineConfig({
   plugins: [
     structureTool({structure}),
     codeInput(),
+    presentationTool({
+      previewUrl: {
+        previewMode: {
+          enable: '/api/draft-mode/enable',
+        },
+      },
+    }),
     // Vision is for querying with GROQ from inside the Studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({defaultApiVersion: apiVersion}),
   ],
+  beta: {
+    create: {
+      startInCreateEnabled: true,
+      fallbackStudioOrigin: "jtrainins.sanity.studio",
+    },
+  },
 })
