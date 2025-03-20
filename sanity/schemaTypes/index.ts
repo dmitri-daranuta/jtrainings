d9ae5378e93ed84f33b2bdb59d1568190a3c8677
Dmitri Daranuta <dmitri.daranuta@ffw.com>
2025-03-20 12:18:35 +0200

    Delete intructor and student types

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,12 +1,10 @@
 import { type SchemaTypeDefinition } from 'sanity';
 import { categoryType } from './categoryType';
 import { enrollmentType } from './enrollmentType';
-import { instructorType } from './instructorType';
 import { lessonCompletionType } from './lessonCompletionType';
 import { lessonType } from './lessonType';
 import { moduleType } from './moduleType';
 import { postType } from './postType';
-import { studentType } from './studentType';
 import { trainingType } from './trainingType';
 import { youtubeType } from './youtubeType';
 import { userType } from './userType';
@@ -15,12 +13,10 @@ export const schema: { types: SchemaTypeDefinition[] } = {
   types: [
     categoryType,
     enrollmentType,
-    instructorType,
     lessonCompletionType,
     lessonType,
     moduleType,
     postType,
-    studentType,
     trainingType,
     youtubeType,
     userType,
